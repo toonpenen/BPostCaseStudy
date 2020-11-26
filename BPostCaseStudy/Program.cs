71e1d016ce1df0ddd9836d74cb0b33e3817ebf46
toonpenen <Toon.Penen@student.intecbrussel.be>
2020-11-26 09:27:35 +0100

    Add project files.

---
+++ BPostCaseStudy/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BPostCaseStudy.Models;
+using System.Threading.Tasks;
+
+
+namespace BPostCaseStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
